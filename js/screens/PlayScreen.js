59c5adab99dccca84ebcf7f665679fe646c90782
ngchitrungkien <trungkienbb17@gmail.com>
2020-10-11 10:06:59 +0700

    update

--- js/screens/PlayScreen.js
+++ js/screens/PlayScreen.js
@@ -51,77 +51,6 @@ const style = /* html */ `
         cursor: pointer;
     }
 
-    /** ranking */
-
-    .ranking {
-        border: 0;
-        background: url(https://images.squarespace-cdn.com/content/v1/52ec28b3e4b0644432d9fd33/1454538036222-JT0VJJKPI23Q2PQ9WHQM/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/IMG_0034.JPG?format=1500w) no-repeat center;
-        background-size: cover;
-        width: 100vw;
-	    min-height: 100vh;
-    }
-    .ranking > header {
-        margin: 0 auto;
-        padding: 1em;
-        text-align: center;
-    }
-    .ranking > header h1 {
-        font-weight: 600;
-        font-size: 3em;
-        margin: 0;
-    }
-    .wrapper {
-        line-height: 1.5em;
-        margin: 0 auto;
-        padding: 2em 0 3em;
-        width: 90%;
-        max-width: 2000px;
-        overflow: hidden;
-    }
-    table {
-        border-collapse: collapse;
-        width: 100%;
-        background: #fff;
-    }
-    th {
-        background-color: #326295;
-        font-weight: bold;
-        color: #fff;
-        white-space: nowrap;
-    }
-    td, th {
-        padding: 1em 1.5em;
-        text-align: left;
-    }
-    tbody th {
-        background-color: #2ea879;
-    }
-    tbody tr:nth-child(2n-1) {
-        background-color: #f5f5f5;
-        transition: all .125s ease-in-out;
-    }
-    tbody tr:hover {
-        background-color: rgba(50,98,149,.3);
-    }
-    td.rank {
-        text-transform: capitalize;
-    }
-    .btn-back {
-        color: #001427;
-        font-family: Titillium Web, sans-serif;
-        font-size: 30px;
-        background: #1bbc9b;
-        width:200px;
-        outline: none;
-        border: none;
-        border-radius: 5px;
-        margin-bottom:15px;
-    }
-    .btn-back:hover {
-        background-color: #007ACC;
-        cursor: pointer;
-    }
-
 </style>
 `;
 
@@ -146,88 +75,13 @@ class PlayScreen extends BaseComponent {
             </form>
         </section>
 
-        <div class="ranking">
-        <header>
-        <h1>Ranking Top 10</h1>
-        </header>
-        <div class="wrapper">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Rank</th>
-                        <th>Name</th>
-                        <th>Points</th>
-                    </tr>
-                </thead>
-            <tbody>
-            <tr>
-                <td class="rank">1</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-                    <tr>
-                <td class="rank">2</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-                <tr>
-                <td class="rank">3</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-                <tr>
-                <td class="rank">4</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-                <tr>
-                <td class="rank">5</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-                <tr>
-                <td class="rank">6</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-                <tr>
-                <td class="rank">7</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-                <tr>
-                <td class="rank">8</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-                <tr>
-                <td class="rank">9</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-            <tr>
-                <td class="rank">10</td>
-                <td class="name"></td>
-                <td class="points"></td>
-            </tr>
-            </tbody>
-            </table>
-            <br>
-            <button class='btn-back'>Back</button>
-        </div>
-        </div>
-
-        
-
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         `;
 
         this.$logOut = this._shadowRoot.querySelector(".btn-log-out");
         this.$play = this._shadowRoot.querySelector(".btn-play");
-        this.$ranking = this._shadowRoot.querySelector(".btn-ranking");
         this.$status = this._shadowRoot.querySelector(".status");
-        this.$back = this._shadowRoot.querySelector(".btn-back");
-
+       
         this.$logOut.onclick = () => {
             localStorage.removeItem("Current-Player")
             localStorage.removeItem("Opponent")
@@ -246,15 +100,6 @@ class PlayScreen extends BaseComponent {
             localStorage.setItem('Current-Player', JSON.stringify(currentPlayer));
             //nen them nut huy o day
         }
-
-        this.$ranking.onclick = () => {
-            var x = this._shadowRoot.querySelector('.play-screen');
-            if (x.style.display === 'none') {
-                x.style.display = 'block';
-            } else {
-                x.style.display = 'none';
-            }
-        };
 
         this.$back.onclick = () => {
             this._shadowRoot.querySelector('.play-screen').style.display = 'block'
